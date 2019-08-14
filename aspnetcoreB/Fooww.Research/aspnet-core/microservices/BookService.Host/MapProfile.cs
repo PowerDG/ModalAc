6c23448a16fc18fdee8447cecaf5b429fff5f1ad
wangshuxin <1049365046@qq.com>
2019-08-14 21:22:40 +0800

    book

---
+++ aspnetcoreB/Fooww.Research/aspnet-core/microservices/BookService.Host/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BookService.Host.Domain;
+using BookService.Host.Domain.Dtos;
+using ResearchService.Host.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.Host
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<BookReview, BookReviewListDto>().ConstructUsing(x => new BookReviewListDto()
+            { MemberName = UserNameHelper.GetUserName(x.LastModifierUserId) });
+
+            CreateMap<Book, BookListDto>().ConstructUsing(x => new BookListDto()
+            { MemberName = UserNameHelper.GetUserName(x.LastModifierUserId) });
+        }
+    }
+}
---
+++ aspnetcoreB/Fooww.Research/research_client/src/views/LibraryPage.vue
@@ -0,0 +1,87 @@
+
+<template>
+    <div class="middle">
+      <div v-if="loginStatus">
+        <book-list></book-list>
+      </div>
+      <div class="no-auth" v-else>
+        <LogoutPage></LogoutPage>
+      </div>
+      <!--<a-menu class="pary-menu"-->
+        <!--@click="handleClick"-->
+        <!--v-model="current"-->
+        <!--mode="horizontal"-->
+      <!--&gt;-->
+        <!--<a-menu-item key="Activity">-->
+          <!--<a-icon type="menu-unfold" />藏云经阁-->
+        <!--</a-menu-item>-->
+        <!--<a-menu-item key="Fund" >-->
+          <!--<a-icon type="interation" />百家讲坛-->
+        <!--</a-menu-item>-->
+      <!--</a-menu>-->
+      <!--<div style="min-height: 500px">-->
+        <!--<router-view></router-view>-->
+      <!--</div>-->
+    </div>
+
+</template>
+
+<script>
+
+    import BookList from "@/components/Libraries/BookList";
+    import LogoutPage from "@/views/Account/LogoutPage";
+    export default {
+      name: "PartyPage",
+      components:{ BookList,LogoutPage},
+      data () {
+        return {
+          current: ['activity'],
+        }
+      },
+      computed:{
+        loginStatus() {
+          return this.$store.state.userInfo.loginStatus;
+        }
+      },
+      methods: {
+        handleClick (e) {
+          console.log('click', e)
+          console.log('click', e.key)
+          this.$router.push({name:e.key})
+        },
+        permissionInit(){
+
+        }
+
+      },
+      created() {
+        console.log("Library Created ",this.loginStatus);
+        if (!this.$store.state.userInfo.loginStatus) {
+          return;
+        }
+        this.permissionInit();
+      },
+    }
+</script>
+
+<style scoped>
+.party{
+ min-height: 700px;
+  max-width: 1280px;
+  margin:0 auto;
+}
+  .pary-menu
+  {
+    max-width: 1280px;
+    margin:0 auto;
+    display: flex;
+    background: none;
+  }
+.no-auth {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+
